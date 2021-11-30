56c83075d264dd9c02564f5d8d8b985869fdd9f9
wstreet <wstreet7@outlook.com>
2021-11-30 23:58:11 +0800

    add gitignore file

--- basic/variable/target/debug/deps/variable-4388dbea4b31179c.d
+++
@@ -1,5 +0,0 @@
-/Users/wangtaidong/www/rust-space/basic/variable/target/debug/deps/variable-4388dbea4b31179c.rmeta: src/main.rs
-
-/Users/wangtaidong/www/rust-space/basic/variable/target/debug/deps/variable-4388dbea4b31179c.d: src/main.rs
-
-src/main.rs:
--- basic/variable/target/debug/deps/variable-dfdc5238fc334767.d
+++
@@ -1,5 +0,0 @@
-/Users/wangtaidong/www/rust-space/basic/variable/target/debug/deps/variable-dfdc5238fc334767.rmeta: src/main.rs
-
-/Users/wangtaidong/www/rust-space/basic/variable/target/debug/deps/variable-dfdc5238fc334767.d: src/main.rs
-
-src/main.rs:
